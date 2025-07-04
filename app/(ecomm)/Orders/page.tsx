5b98c1f2e97d7be1a61e84f2d363d3d8529d3397
Ahmed0Idriss0dev <webocamw224@gmail.com>
2025-07-04 17:46:26 +0100

    add icons

--- app/(ecomm)/Orders/page.tsx
+++ app/(ecomm)/Orders/page.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-        <h1>hello world</h1>
+    <div className='w-full h-dvh'>
+       <div className="w-full flex-col h-full flex justify-center items-center">
+         <Image src='/order.svg' className='w-60 h-60' width={400} height={400} alt='logo' />
+         <p>🛒 No orders yet.</p>
+       </div>
     </div>
   )
 }
--- app/(ecomm)/Settings/page.tsx
+++ app/(ecomm)/Settings/page.tsx
@@ -1,8 +1,22 @@
+import { currentUser } from '@clerk/nextjs/server'
+import { CopyPlusIcon } from 'lucide-react'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+  const user = await currentUser()
   return (
-    <div>page</div>
+    <div className='flex flex-col gap-2'>
+      <div className="">
+        <h3 className='text-[20px] '>Store acess key</h3>
+        <p> Please keep it confidential to ensure the safety of your store.</p>
+      </div>
+      <div className="w-96 px-2.5 text-sm flex justify-between items-center h-11 bg-brand-50 border border-neutral-200 rounded-md">
+        <p>{user?.id} </p>
+        <button>
+          <CopyPlusIcon className='text-brand-500'/>
+        </button>
+      </div>
+    </div>
   )
 }
 
