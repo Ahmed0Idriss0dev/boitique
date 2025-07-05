3e1c67764f84c733ea93d9391b63bbf26b4a333e
Ahmed0Idriss0dev <webocamw224@gmail.com>
2025-07-05 18:24:19 +0100

    fix dir

--- app/api/remove/route.ts
+++ app/api/remove/route.ts
@@ -1,5 +1,3 @@
-import { Prisma } from "@/lib/generated/prisma";
-import prisma from "@/utils/client";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath, revalidateTag } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
@@ -7,14 +5,5 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const { userId } = await auth();
   if (!userId) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-
-  const user = await currentUser()
-  const data = await req.json()
-  const users = user?.id
-  const { id} = data
-  const products = await prisma.products.delete({
-    where:{id}
-  })
-  revalidateTag('collection')  
   return Response.json( { status: 200 })
 }
