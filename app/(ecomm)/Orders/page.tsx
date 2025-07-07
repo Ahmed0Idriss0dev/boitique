b0e7ae9be233ee8c21683a577bd12f7cbcba91e3
Ahmed0Idriss0dev <webocamw224@gmail.com>
2025-07-07 13:59:27 +0100

    fix add

--- app/(ecomm)/Orders/page.tsx
+++ app/(ecomm)/Orders/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div className='w-full h-dvh'>
        <div className="w-full flex-col h-full flex justify-center items-center">
-         <Image src='/order.svg' className='w-60 h-60' width={400} height={400} alt='logo' />
+         <Image src='/icons/Order.svg' className='w-40 h-40' width={400} height={400} alt='logo' />
          <p>🛒 No orders yet.</p>
        </div>
     </div>
