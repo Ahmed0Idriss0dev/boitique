6b5fa198ec2e4b7c4c2ffaf4c174d9fe5cdbbc23
Ahmed0Idriss0dev <webocamw224@gmail.com>
2025-07-07 12:48:14 +0100

    add and save

--- app/api/get-products/route.ts
+++ app/api/get-products/route.ts
@@ -1,8 +1,11 @@
 export const runtime = 'nodejs'; 
 import { NextResponse } from 'next/server'
-import { currentUser } from '@clerk/nextjs/server'
+import { auth, currentUser } from '@clerk/nextjs/server'
+import { GetProducts } from '@/client/db/curd';
 
 export async function POST(req: Request) {
-  const { userId } :{userId:string} = await req.json()
-   return NextResponse.json({data:[]})
+      const { userId } = await auth();
+      if (!userId) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+       const data = GetProducts()
+      return NextResponse.json({data})
   }
--- components/section/Products.tsx
+++ components/section/Products.tsx
@@ -1,34 +1,52 @@
+'use client'
 import { Product } from '@/types'
 import { MapPin, MoreVertical, Truck } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
-import Remove from '../kits/Remove'
+import React, { useEffect, useState } from 'react'
 
 import { GetProducts } from '@/client/db/curd'
 import ProductCard from '../kits/ProductCard'
 
 
-const Products = async  () => {
-      const data:Product[] = await  GetProducts()
+const Products = () => {
+    const [data , setdata] = useState([])
+    useEffect(() => {
+        async function get() {
+            try {
+                const response = await fetch(`/api/get-products`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                })
+                const data = await response.json()
+                console.log(data)
+            } catch (error) {
+
+            }
+        }
+        get()
+    }, [])
+    const results = data.filter((item) => item.currency.startsWith("E"));
     return (
         <>
-        {
-        data[0] ? (
-        <div className='flex flex-col gap-1 '>
             {
-                data.map(({ price, description, title, Delivery   ,ProductImage , $id ,currency}, i) => (
-                  <ProductCard currency={currency} description={description} id={$id} userId={''} key={i} Delivery={Delivery} title={title} ProductImage={ProductImage} price={price}  />
-                ))
+                data[0] ? (
+                    <div className='flex flex-col gap-1 '>
+                        {
+                            results.map(({ price, description, title, Delivery, ProductImage, $id, currency }, i) => (
+                                <ProductCard currency={currency} description={description} id={$id} userId={''} key={i} Delivery={Delivery} title={title} ProductImage={ProductImage} price={price} />
+                            ))
+                        }
+                    </div>
+                ) :
+                    <div className="flex w-full h-full justify-center items-center">
+                        <Image src='/logo/noProduct.svg' className='w-50' width={400} height={400} alt='logo' />
+                    </div>
             }
-        </div>
-            ) :
-            <div className="flex w-full h-full justify-center items-center">
-                <Image src='/logo/noProduct.svg' className='w-50'  width={400} height={400} alt='logo'/>
-            </div>
-        }
-        
+
         </>
-        )
+    )
 }
 
 export default Products
