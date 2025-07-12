e5ca0f4957dfcc34db91f12fa4ec986b24ef92c4
Ahmed0Idriss0dev <webocamw224@gmail.com>
2025-07-12 12:11:42 +0100

    push chages

--- app/api/store/route.ts
+++ app/api/store/route.ts
@@ -2,10 +2,16 @@ export const runtime = 'nodejs';
 import { NextResponse } from 'next/server'
 import { currentUser } from '@clerk/nextjs/server'
 import { GetProductsStore } from '@/client/db/curd';
-
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-key',
+};
 export async function POST(req: Request) {
    const AccessKey =  req.headers.get('Access-key')
    if(!AccessKey) return NextResponse.json({status:401})
    const Product = await GetProductsStore(AccessKey)
-   return NextResponse.json({Product})
-  }
+return new NextResponse(JSON.stringify({ Product }), {
+    status: 200,
+    headers: CORS_HEADERS,
+  });  }
